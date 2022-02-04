79a059f155949501956b5124e07dc5bdbf47c0f8
remimimimi <valent.xarin@gmail.com>
2022-02-04 03:40:14 +0300

    Move to ssreflect tactics

--- theorems/Peano.v
+++ theorems/Peano.v
@@ -1,6 +1,6 @@
 (* Require Import Bool Arith List Cpdt.CpdtTactics. *)
 (* Require Import Cpdt.CpdtTactics. *)
-Require Import Bool List.
+Require Import Bool List Vectors.Vector ssreflect.
 Set Implicit Arguments.
 Set Asymmetric Patterns.
 
@@ -21,74 +21,44 @@ Axiom a_plus_one : forall a, a +++ S O = S a.
 Axiom a_plus_s_b : forall a b, a +++ S b = S (a +++ b).
 
 Lemma o_plus_a : forall a, O +++ a = a.
-  intros.
-  unfold plus.
-  reflexivity.
-Qed.
+Proof. done. Qed.
 
 Lemma a_plus_o : forall a, a +++ O = a.
-  intros.
-  induction a.
-  reflexivity.
-  simpl.
-  rewrite IHa.
-  reflexivity.
-Qed.
+Proof. elim => [| a IHa]; by [|simpl; rewrite IHa]. Qed.
+
+Example two_plus_two : S (S O) +++ S (S (S O)) = S (S (S (S (S O)))).
+Proof. done. Qed.
 
 Lemma plus_comm : forall a b, a +++ b = b +++ a.
-  intros.
-  induction a.
-  symmetry.
-  rewrite a_plus_o.
-  reflexivity.
-  symmetry.
-  rewrite a_plus_s_b.
-  rewrite <- IHa.
-  reflexivity.
+Proof.
+  elim => [| a IHa] b;
+    by [rewrite a_plus_o | rewrite a_plus_s_b -IHa].
 Qed.
 
 Lemma s_a_plus_b : forall a b, S a +++ b = S (a +++ b).
-  intros.
-  induction a.
-  reflexivity.
-  symmetry.
-  rewrite plus_comm.
-  rewrite a_plus_s_b.
-  simpl.
-  rewrite plus_comm.
-  reflexivity.
+Proof.
+  elim => [| ? ?] ?; by [trivial].
 Qed.
 
 Lemma plus_assoc : forall a b c, a +++ (b +++ c) = (a +++ b) +++ c.
-  intros.
-  induction b.
-  rewrite o_plus_a.
-  rewrite a_plus_o.
-  reflexivity.
-  simpl.
-  rewrite a_plus_s_b.
-  rewrite IHb.
-  symmetry.
-  rewrite <- a_plus_one.
-  rewrite plus_comm.
-  rewrite a_plus_s_b.
-  rewrite a_plus_s_b.
-  rewrite plus_comm.
-  rewrite s_a_plus_b.
-  rewrite a_plus_o.
-  reflexivity.
+Proof.
+  move => a b c; move : b.
+  elim => [| b IHb].
+  - by rewrite a_plus_o.
+  - by rewrite a_plus_s_b;
+       do 2! rewrite s_a_plus_b;
+       rewrite  a_plus_s_b;
+       rewrite IHb.
 Qed.
 
-(* Lemma s_a_eq_s_b : forall a b, S a = S b -> a = b. *)
-(*   intros. *)
-(* Qed. *)
+Lemma s_a_eq_s_b : forall a b, S a = S b -> a = b.
+  (* TODO: replace inversion by ssreflect more natural (haha) approach *)
+  move => a b succ_eq; by inversion succ_eq.
+Qed.
 
-(* Lemma s_a_eq_s_b : forall a b, a = b -> S a = S b. *)
-(*   intros. *)
-(*   rewrite <- a_plus_one. *)
-(*   symmetry. *)
-(*   rewrite <- a_plus_one. *)
-(* Qed. *)
+Lemma a_eq_b : forall a b, a = b -> S a = S b.
+  move => a b abeq; by rewrite abeq.
+Qed.
 
 Fixpoint mul (n : natural) (m : natural) : natural :=
   match n with
@@ -101,84 +71,45 @@ Axiom a_mul_o : forall a, a *** O = O.
 Axiom a_mul_s_b : forall a b, a *** S b = a +++ (a *** b).
 
 Lemma o_mul_a : forall a, O *** a = O.
-  intro.
-  reflexivity.
-Qed.
+Proof. done. Qed.
 
 Lemma a_mul_s_o : forall a, a *** S O = a.
-  intro.
-  induction a.
-  reflexivity.
-  simpl.
-  rewrite IHa.
-  reflexivity.
+Proof.
+  elim => [| a IHa]; by [|rewrite -{2}IHa].
 Qed.
 
 Lemma s_o_mul_a : forall a, S O *** a = a.
-  intro.
-  simpl.
-  rewrite a_plus_o.
-  reflexivity.
+Proof.
+  move => a; by simpl; rewrite a_plus_o.
 Qed.
 
 Lemma mul_comm : forall a b, a *** b = b *** a.
-  intros.
-  induction a.
-  simpl.
-  rewrite a_mul_o.
-  reflexivity.
-  symmetry.
-  rewrite a_mul_s_b.
-  rewrite <- IHa.
-  simpl.
-  reflexivity.
+Proof.
+  elim => [| a IHa] b; by [rewrite a_mul_o | rewrite a_mul_s_b -IHa].
 Qed.
 
 Lemma s_a_mul_b : forall a b, S a *** b = b +++ (b *** a).
-  intros.
-  induction a.
-  simpl.
-  symmetry.
-  rewrite a_mul_o.
-  reflexivity.
-  simpl.
-  rewrite a_mul_s_b.
-  rewrite mul_comm.
-  reflexivity.
+Proof.
+  elim => [| a IHa] b;
+    by simpl; by [rewrite a_mul_o | rewrite a_mul_s_b mul_comm].
 Qed.
 
 Lemma mul_distr : forall a b c, a *** (b +++ c) = a *** b +++ a *** c.
-  intros.
-  (* induction a. *)
-  (* reflexivity. *)
-  (* simpl. *)
-
-  induction b.
-  simpl.
-  rewrite a_mul_o.
-  reflexivity.
-  rewrite a_mul_s_b.
-  symmetry.
-  rewrite <- plus_assoc.
-  rewrite <- IHb.
-  rewrite <- a_mul_s_b.
-  rewrite <- s_a_plus_b.
-  reflexivity.
+Proof.
+  move => a b c; move : b.
+  elim => [| b IHb].
+  - by rewrite a_mul_o.
+  - by rewrite a_mul_s_b -plus_assoc -IHb -a_mul_s_b -s_a_plus_b.
 Qed.
 
 Lemma mul_assoc : forall a b c, a *** (b *** c) = (a *** b) *** c.
-  intros.
-  induction c.
-  rewrite a_mul_o.
-  rewrite a_mul_o.
-  rewrite a_mul_o.
-  reflexivity.
-  rewrite a_mul_s_b.
-  rewrite a_mul_s_b.
-  symmetry.
-  rewrite mul_comm.
-  rewrite mul_distr.
-  rewrite mul_comm.
-  rewrite IHc.
-  reflexivity.
+Proof.
+  move => a b c; elim: c => [| c IHc].
+  - by do 3! rewrite a_mul_o.
+  - by do 2! rewrite a_mul_s_b;
+       symmetry;
+       rewrite mul_comm;
+       rewrite mul_distr;
+       rewrite mul_comm;
+       rewrite IHc.
 Qed.
